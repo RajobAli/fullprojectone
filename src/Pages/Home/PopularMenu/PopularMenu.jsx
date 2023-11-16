8620532ae853b47f30ec43bd5993365e72e9c08f
RajobAli <freelancerrajobali@gmail.com>
2023-11-16 19:18:40 +0600

    adding custom hooks

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,41 +1,44 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import UseMenu from "../../../hooks/UseMenu";
 
 
 
 const PopularMenu = () => {
+   const [menu] = UseMenu();
+   const popular = menu.filter(item =>item.category === 'popular');
 
-    const [menu,setMenu] = useState([]);
+   // const [menu, setMenu] = useState([]);
 
- useEffect(() =>{
-    fetch('menu.json')
-    .then(res =>res.json())
-    .then(data =>{
-        const popularItems = data.filter(item =>item.category ==='popular');
-        setMenu(popularItems)
-    })
- 
- },[])
+   // useEffect(() => {
+   //    fetch('menu.json')
+   //       .then(res => res.json())
+   //       .then(data => {
+   //          const popularItems = data.filter(item => item.category === 'popular');
+   //          setMenu(popularItems)
+   //       })
 
-    return (
-        
- <section>
+   // }, [])
+
+   return (
+
+      <section>
          <SectionTitle
-      heading="From Our Menu"
-      subHeading="Popular Items"
-      >
-        
-      </SectionTitle>
-      <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
-        {
-            menu.map(item =><MenuItem key={item._id} item={item}></MenuItem>)
-        }
-      </div>
+            heading="From Our Menu"
+            subHeading="Popular Items"
+         >
 
- </section>
-     
-    );
+         </SectionTitle>
+         <div className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
+            {
+               popular.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+            }
+         </div>
+
+      </section>
+
+   );
 };
 
 export default PopularMenu;
