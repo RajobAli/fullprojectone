22c3cf196576ad914c682a87651654e081eefe1b
RajobAli <freelancerrajobali@gmail.com>
2023-11-17 19:07:12 +0600

    adding tabulation

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -3,8 +3,16 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
 import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import UseMenu from '../../../hooks/UseMenu';
 
 const Menu = () => {
+    const [menu] = UseMenu();
+    const desserts = menu.filter(item =>item.category === 'dessert');
+    const soup = menu.filter(item =>item.category === 'soup');
+    const salad = menu.filter(item =>item.category === 'salad');
+    const pizza = menu.filter(item =>item.category === 'pizza');
+    const offered = menu.filter(item =>item.category === 'offered');
+
     return (
         <div>
             <Helmet>
